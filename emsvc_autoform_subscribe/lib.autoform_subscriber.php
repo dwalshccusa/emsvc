7c477da7dd410ff7754e88d1a70c07e4ddc4e568
mvcuccaro <mvcuccaro@gmail.com>
2017-01-09 01:21:08 -0500

    adding subscribe files

---
+++ emsvc_autoform_subscribe/lib.autoform_subscriber.php
@@ -0,0 +1,83 @@
+<?php
+/**
+* simple utility class for subscribing an emsvc member to a list via an autoform id and key
+* email is always required - additional fields will be determined by the setup of the autoform
+* on the emsvc application - any fields not declared in the autoform setup will not be validated
+* and will not be stored
+*/
+
+class autoform_subscriber
+{
+	public $autoform_url	= 'https://enflyer.emsvc.com/autoform_display.php';
+	public $post_query		= '';
+
+
+	public function __construct()
+	{
+
+	}
+
+	public function setAutoformUrl($arg_url)
+	{
+		$this->autoform_url 	= $arg_url;
+	}
+
+	public function setAutoformId($arg_autoform_id)
+	{
+		$this->fields['autoform_id'] 	= $arg_autoform_id;
+	}
+
+	public function setAutoformKey($arg_autoform_key)
+	{
+		$this->fields['af_key'] 		= $arg_autoform_key;
+	}
+
+	public function addField($arg_name, $arg_value)
+	{
+		$this->fields[$arg_name] = $arg_value;
+	}
+
+	public function subscribe()
+	{
+		$curl	= curl_init($this->autoform_url);
+		$post_query = $this->buildQuery();
+
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: application/x-www-form-urlencoded"));
+		curl_setopt($curl, CURLOPT_POST, 1);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $post_query);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		// If you experience SSL issues, perhaps due to an outdated SSL cert
+  		// on your own server, try uncommenting the line below
+ 		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+		curl_exec($curl);
+		curl_close($curl);
+		sleep(2); //hitting the endpoint too frequently can result in an http ban on the server
+
+	}
+
+	private function buildQuery()
+	{
+		if( !isset($this->fields['autoform_id']) )
+		{
+			die('you must set an autoform id with function setAutoformId()');
+		}
+
+		if( !isset($this->fields['af_key']) )
+		{
+			die('you must set an autoform key with function setAutoformKey()');
+		}
+
+		if( !isset($this->fields['email']) )
+		{
+			die('all autoform subscription requests require at least an email field be set with the addField function');
+		}
+
+		$post_query = 'action=add&';
+		foreach($this->fields as $key => $value)
+		{
+			$post_query .= 'fields[' . $key . ']=' . $value  . '&';
+		}
+		return $post_query;
+	} 
+}
+?>
---
+++ emsvc_autoform_subscribe/subscribe.php
@@ -0,0 +1,23 @@
+<?
+/**
+* example app for using the autoform_subscriber utility class
+* use: subscribe.php --autoform_id 122345 --autoform_key jkh52jh8 --email someemail@somedomain.com
+*/
+INCLUDE_ONCE('lib.autoform_subscriber.php');
+
+$shortopts = '';
+$longopts = array(
+			'autoform_id:',
+			'autoform_key:',
+			'email:'
+);
+
+$cli_args	= getopt($shortopts, $longopts);
+
+$subscriber = new autoform_subscriber();
+$subscriber->setAutoformUrl('http://enflyer.emsvc.com/autoform_display.php');
+$subscriber->setAutoformId($cli_args['autoform_id']);
+$subscriber->setAutoformKey($cli_args['autoform_key']);
+$subscriber->addField('email', $cli_args['email']);
+$subscriber->subscribe();
+?>
