9187eaab0d8351a571bd2306c33449c6d89ec403
mvcuccaro <mvcuccaro@gmail.com>
2017-02-07 02:25:16 -0500

    name change fixes

--- wordpress_plugins/enflyer_subscribe/enflyer_wp_subscribe.php
+++ wordpress_plugins/enflyer_subscribe/enflyer_wp_subscribe.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: Enflyer Subscribe
+ * Plugin Name: Enflyer Auto Subscribe
  * Plugin URI: http://www.enflyer.com
  * Description: When the Enflyer Subscription plugin is enabled, all valid users who signup for your site will be immedaitely subscribed to your Enflyer memberlist via a hidden signup for that you have set up in your Enflyer account.
  * Version: 1.0.0
@@ -18,9 +18,9 @@ function enflyer_auto_subscribe($arg_user_id)
         /**
         * retrieve plugin settings
         */
-        $plugin_settings        = get_option('enflyer_auto_ssettings');
-        $autoform_id            = $plugin_settings['enflyer_auto_stext_field_0'];   //the unique id of the enflyer autoform
-        $autoform_key           = $plugin_settings['enflyer_auto_stext_field_1'];   //the validation key of the enflyer autoform
+        $plugin_settings        = get_option('enflyer_auto_s_settings');
+        $autoform_id            = $plugin_settings['enflyer_auto_s_text_field_0'];   //the unique id of the enflyer autoform
+        $autoform_key           = $plugin_settings['enflyer_auto_s_text_field_1'];   //the validation key of the enflyer autoform
         error_log('plugin_settings - autoform_id: '     . $autoform_id);
         error_log('plugin_settings - autoform_key: '    . $autoform_key);
 
@@ -66,80 +66,79 @@ function enflyer_auto_subscribe($arg_user_id)
 * Begin Plugin Settings
 * thanks to http://wpsettingsapi.jeroensormani.com/ for making it easy to generate plugin settings code.
 */
-add_action( 'admin_menu', 'enflyer_auto_sadd_admin_menu' );
-add_action( 'admin_init', 'enflyer_auto_ssettings_init' );
+add_action( 'admin_menu', 'enflyer_auto_s_add_admin_menu' );
+add_action( 'admin_init', 'enflyer_auto_s_settings_init' );
 
 
-function enflyer_auto_sadd_admin_menu(  ) {
+function enflyer_auto_s_add_admin_menu(  ) {
 
-        add_options_page( 'Enflyer Subscribe', 'Enflyer Subscribe', 'manage_options', 'enflyer_auto_subscribe', 'enflyer_auto_soptions_page' );
-
+        add_options_page( 'Enflyer Auto Subscribe', 'Enflyer Auto Subscribe', 'manage_options', 'enflyer_auto_subscribe', 'enflyer_auto_soptions_page' );
 }
 
-function enflyer_auto_ssettings_init(  ) {
+function enflyer_auto_s_settings_init(  ) {
 
-        register_setting( 'pluginPage', 'enflyer_auto_ssettings' );
+        register_setting( 'pluginPage', 'enflyer_auto_s_settings' );
 
         add_settings_section(
-                'enflyer_auto_spluginPage_section',
+                'enflyer_auto_s_pluginPage_section',
                 __( 'When the Enflyer Subscription plugin is enabled, all valid users who signup for your site will be immedaitely subscribed to your Enflyer memberlist via a hidden signup for that you have set up in your Enflyer account.', 'wordpress' ),
-                'enflyer_auto_ssettings_section_callback',
+                'enflyer_auto_s_settings_section_callback',
                 'pluginPage'
         );
 
         add_settings_field(
-                'enflyer_auto_stext_field_0',
+                'enflyer_auto_s_text_field_0',
                 __( 'autoform id', 'wordpress' ),
                 'enflyer_auto_stext_field_0_render',
                 'pluginPage',
-                'enflyer_auto_spluginPage_section'
+                'enflyer_auto_s_pluginPage_section'
         );
 
         add_settings_field(
-                'enflyer_auto_stext_field_1',
+                'enflyer_auto_s_text_field_1',
                 __( 'autoform key', 'wordpress' ),
-                'enflyer_auto_stext_field_1_render',
+                'enflyer_auto_s_text_field_1_render',
                 'pluginPage',
-                'enflyer_auto_spluginPage_section'
+                'enflyer_auto_s_pluginPage_section'
         );
 
 
 }
 
 
-function enflyer_auto_stext_field_0_render(  ) {
+function enflyer_auto_s_text_field_0_render(  ) {
 
-        $options = get_option( 'enflyer_auto_ssettings' );
+        $options = get_option( 'enflyer_auto_s_settings' );
         ?>
-        <input type='text' name='enflyer_auto_ssettings[enflyer_auto_stext_field_0]' value='<?php echo $options['enflyer_auto_stext_field_0']; ?>'>
+        <input type='text' name='enflyer_auto_s_settings[enflyer_auto_s_text_field_0]' value='<?php echo $options['enflyer_auto_s_text_field_0']; ?>'>
         <?php
 
 }
 
 
-function enflyer_auto_stext_field_1_render(  ) {
+function enflyer_auto_s_text_field_1_render(  ) {
 
-        $options = get_option( 'enflyer_auto_ssettings' );
+        $options = get_option( 'enflyer_auto_s_settings' );
         ?>
-        <input type='text' name='enflyer_auto_ssettings[enflyer_auto_stext_field_1]' value='<?php echo $options['enflyer_auto_stext_field_1']; ?>'>
+        <input type='text' name='enflyer_auto_s_settings[enflyer_auto_s_text_field_1]' value='<?php echo $options['enflyer_auto_s_text_field_1']; ?>'>
         <?php
 
 }
 
 
-function enflyer_auto_ssettings_section_callback(  ) {
+function enflyer_auto_s_settings_section_callback(  ) {
 
         echo __( 'The autoform id and autoform key are required for subscribing a member to an autoform (sign up form)', 'wordpress' );
 
 }
 
 
-function enflyer_auto_soptions_page(  ) {
+function enflyer_auto_s_options_page(  ) {
 
         ?>
         <form action='options.php' method='post'>
 
-                <h2>Enflyer Subscribe</h2>
+                <h2>Enflyer Auto Subscribe</h2>
 
                 <?php
                 settings_fields( 'pluginPage' );
